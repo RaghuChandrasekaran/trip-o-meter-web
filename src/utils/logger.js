b05c955d8e100ebc94081ea4fdfb140ceb9a0a4d
Raghu <raghu.c@appviewx.com>
2017-03-02 16:13:08 +0530

    Logging Improvements

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,39 @@
 import winston from 'winston';
+import fs from 'fs';
 
-const TEST_ENV = process.env.NODE_ENV === 'test';
-const defaultLvl = (__DEV__ && !TEST_ENV) ? 'debug' : 'info';
+const defaultLvl = __DEV__ ? 'debug' : 'info';
 const LOG_LEVEL = process.env.LOG_LEVEL || defaultLvl;
+const MAX_SIZE = 5000000;
+const MAX_FILES = 5;
+const INFO_LOG = 'app-logs.log';
+const logDir = 'logs';
+
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
 
 winston.level = LOG_LEVEL;
+
+const consoleTransportOpts = {
+  level: LOG_LEVEL,
+  colorize: true,
+};
+const fileTransportOpts = {
+  level: LOG_LEVEL,
+  name: 'File',
+  filename: `${logDir}/${INFO_LOG}`,
+  maxsize: MAX_SIZE,
+  maxFiles: MAX_FILES,
+  handleExceptions: true,
+  humanReadableUnhandledException: true,
+};
+
+winston.configure({
+  transports: [
+    new (winston.transports.Console)(consoleTransportOpts),
+    new (winston.transports.File)(fileTransportOpts),
+  ],
+});
 
 function Logger(metadata) {
   this.metadata = metadata;
@@ -16,6 +45,9 @@ function Logger(metadata) {
   };
   this.debug = (message) => {
     winston.log('debug', message, this.metadata);
+  };
+  this.verbose = (message) => {
+    winston.log('verbose', message, this.metadata);
   };
 }
 
